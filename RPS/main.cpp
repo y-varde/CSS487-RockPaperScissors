67f9b83a640fab639298de1011051f78374da82f
yashv3 <yashv3@uw.edu>
2023-11-25 03:35:28 -0800

    Initial codebase (make sure props file and dlls are configured)

---
+++ RPS/main.cpp
@@ -0,0 +1,23 @@
+// main.cpp
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/matx.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+
+//main for rock paper scissors
+int main(int argc, char* argv[])
+{
+    //read in image with user
+    //let user know what they picked
+    //pick random number for computer
+    //compare user and computer results
+    //display results
+    //ask user if they want to play again
+}
