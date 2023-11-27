ab751b200d491920d52ec305ce23e540e82c0c95
yashv3 <yashv3@uw.edu>
2023-11-27 14:25:08 -0800

    Surrounding functionality implemented. Need to still add image recognition code

--- RPS/main.cpp
+++ RPS/main.cpp
@@ -9,15 +9,99 @@
 #include <iostream>
 
 using namespace cv;
+using namespace std;
 
+//play game
+//takes in the text from the image and plays rock paper scissors
+void playGame(String text)
+{
+    String choices[3] = { "rock", "paper", "scissors" };
+    String aiChoice = choices[rand() % 3];
+    String userChoice = "";
+    if (text == "rock" || text == "paper" || text == "scissors")
+    {
+        userChoice = text;
+    }
+    else
+    {
+        cout << "Invalid choice. Please choose either rock, paper, or scissors." << endl;
+        return;
+    }
+    if (userChoice == aiChoice)
+    {
+        cout << "It's a draw!" << endl;
+    }
+    else if ((userChoice == "rock" && aiChoice == "scissors") || (userChoice == "scissors" && aiChoice == "paper") || (userChoice == "paper" && aiChoice == "rock"))
+    {
+        cout << "You win!" << endl;
+    }
+    else
+    {
+        cout << "AI wins!" << endl;
+    }
+}
+
+//image to string
+//takes in an image and returns the text in the image
+String imageToString(Mat image)
+{
+    String text = "";
+    //IMPLEMENTATION GOES HERE
+    return text;
+}
+
+//process image
+Mat processImage(Mat image)
+{
+    Mat gray;
+    cvtColor(image, gray, COLOR_BGR2GRAY);
+    Mat thresholded;
+    threshold(gray, thresholded, 30, 255, THRESH_BINARY);
+    imshow("img", thresholded);
+    waitKey(0);
+    return thresholded;
+}
+
+//capture photo
+Mat capturePhoto()
+{
+    VideoCapture cap(0);
+    Mat frame;
+    while (true)
+    {
+        cap >> frame;
+        imshow("img", frame);
+        if (waitKey(1) == 'q')
+        {
+            break;
+        }
+    }
+    cap.release();
+    destroyAllWindows();
+    return frame;
+}
 
 //main for rock paper scissors
 int main(int argc, char* argv[])
 {
-    //read in image with user
-    //let user know what they picked
-    //pick random number for computer
-    //compare user and computer results
-    //display results
-    //ask user if they want to play again
+    bool playAgain = true;
+    while (playAgain)
+    {
+        Mat image = capturePhoto();
+        image = processImage(image);
+        String text = imageToString(image);
+        playGame(text);
+        cout << "Would you like to play again? (y/n)" << endl;
+        String answer;
+        cin >> answer;
+        if (answer == "n")
+        {
+            playAgain = false;
+        }
+    }
+
 }
+
+
+
+    
