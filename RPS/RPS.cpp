255c29be35257cd67096c01d7fab0484da472267
yashv3 <yashv3@uw.edu>
2023-12-04 03:51:54 -0800

    added some additional image processing to try and detect gesture better

--- RPS/RPS.cpp
+++ RPS/RPS.cpp
@@ -1,4 +1,4 @@
-// main.cpp
+// RPS.cpp
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/core/mat.hpp>
@@ -95,25 +95,55 @@ Mat processImage(Mat image)
     // Convert the image to grayscale
     Mat gray;
     cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    imshow("Grayscale", gray);
+    waitKey(0);
 
     // Apply Gaussian blur to the grayscale image
     Mat blur;
     GaussianBlur(gray, blur, cv::Size(5, 5), 0);
-
-    // Apply Canny edge detection
-    Mat edges;
-    Canny(blur, edges, 50, 150);
-
-    // Dilate the edges
-    Mat dilatedEdges;
-    dilate(edges, dilatedEdges, Mat());
-
-    // Display the edge image
-    namedWindow("Edges", cv::WINDOW_NORMAL);
-    imshow("Edges", dilatedEdges);
+    imshow("Gaussian Blur", blur);
     waitKey(0);
 
-    return dilatedEdges;
+    //apply thresholding
+    Mat thresh;
+    threshold(blur, thresh, 0, 255, THRESH_BINARY_INV + THRESH_OTSU);
+    imshow("Threshold", thresh);
+    waitKey(0);
+
+    //find contours
+    vector<vector<Point>> contours;
+    findContours(thresh, contours, RETR_LIST, CHAIN_APPROX_NONE);
+
+    //get the hand contour (assume the largest contour is the hand)
+    int largestContourIndex = 0;
+    for (int i = 0; i < contours.size(); i++)
+    {
+        if (contourArea(contours[i]) > contourArea(contours[largestContourIndex]))
+        {
+            largestContourIndex = i;
+        }
+    }
+
+    //create a mask of the hand
+    Mat mask = Mat::zeros(image.size(), CV_8UC1);
+    drawContours(mask, contours, largestContourIndex, Scalar(255), FILLED);
+    imshow("Mask", mask);
+    waitKey(0);
+
+    //apply the mask to the image
+    Mat maskedImage;
+    image.copyTo(maskedImage, mask);
+    imshow("Masked Image", maskedImage);
+    waitKey(0);
+
+    // Dilate the hand
+    Mat dilated;
+    dilate(mask, dilated, Mat(), Point(-1, -1), 3);
+    imshow("Dilated", dilated);
+    waitKey(0);
+
+    //return the dilated image
+    return dilated;
 }
 
 //capture photo
@@ -164,7 +194,6 @@ int main(int argc, char* argv[])
             playAgain = false;
         }
     }
-
 }
 
 
